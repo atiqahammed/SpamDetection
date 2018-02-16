485da24592908d69bdb77a5d50c36584624c2277
atiqahammed <atiqahammedshamim@gmail.com>
2018-02-16 11:43:07 +0600

    spam detection is completed

---
+++ SpamDetection-Test003/src/main/Test.java
@@ -0,0 +1,12 @@
+package main;
+
+import detector.SpamDetector;
+
+public class Test {
+	public static void main(String[] args) {
+		SpamDetector detector = new SpamDetector("spamData.txt");
+		detector.inputData();
+		//System.out.println(detector.getAccuracy());
+		detector.test();
+	}
+}
